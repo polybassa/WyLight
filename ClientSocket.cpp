6e9bc810792a866eee9f40b2ff1c31355fda4f32
Patrick Brünn <taw@bruenn.biz>
2012-06-18 00:30:34 +0200

    added udp client to send set color commands threw udp socket to simulated isr

---
+++ ClientSocket.cpp
@@ -0,0 +1,45 @@
+#include "ClientSocket.h"
+#include "commandstorage.h"
+
+#include <cstring>
+#include <iostream>
+
+ClientSocket::ClientSocket(long addr, short port)
+	: mSock(socket(AF_INET, SOCK_DGRAM, 0))
+{
+	memset(&mSockAddr, 0, sizeof(mSockAddr));
+	mSockAddr.sin_family = AF_INET;
+	mSockAddr.sin_port = htons(port);
+	mSockAddr.sin_addr.s_addr = htonl(addr);
+}
+
+ClientSocket::~ClientSocket()
+{
+	close(mSock);
+}
+
+int ClientSocket::Send(char* buf, size_t length)
+{
+	return sendto(mSock, buf, length, 0, (struct sockaddr*)&mSockAddr, sizeof(mSockAddr));
+}
+
+void colorLoop(ClientSocket& socket)
+{
+	static const size_t NUM_COLORS = 3;
+	char* buffer[NUM_COLORS] = {cmd_set_color_red, cmd_set_color_green, cmd_set_color_blue};
+	for(int i = 0;;i++)
+	{
+		int bytesWritten = socket.Send(buffer[i % NUM_COLORS], sizeof(cmd_set_color_red));
+		std::cout << "Send " << bytesWritten << " bytes" << std::endl;
+
+		//wait a second
+		sleep(1);
+	}
+}
+
+int main(int argc, const char* argv[])
+{
+	ClientSocket socket(INADDR_ANY, 12345);
+	colorLoop(socket);
+	return 0;
+}
--- Makefile
+++ Makefile
@@ -9,7 +9,10 @@ pic:
 	wine ${PIC_CC} main.c -Iinclude_files
 
 x86:
-	gcc main.c commandstorage.c eeprom.c error.c ledstrip.c RingBuf.c spi.c usart.c x86_wrapper.c -DX86 -DNO_CRC -I${INC_DIR} -lpthread ${OPENGL_LIB}
+	gcc main.c commandstorage.c eeprom.c error.c ledstrip.c RingBuf.c spi.c usart.c x86_wrapper.c -DX86 -DNO_CRC -I${INC_DIR} -lpthread ${OPENGL_LIB} -o server.bin
+
+client:
+	g++ ClientSocket.cpp -DX86 -I${INC_DIR} -o client.bin -Wall
 
 clean:
 	rm *.hex *.occ *.txt *.out
