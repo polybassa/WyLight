dced088e84c663d10449901255ce4f7bc5850967
gpb <taw@bruenn.biz>
2012-11-18 19:09:36 +0100

    added unittest skeleton for ledstrip

---
+++ ledstrip_ut.c
@@ -0,0 +1,47 @@
+/**
+ Copyright (C) 2012 Nils Weiss, Patrick Bruenn.
+ 
+ This file is part of Wifly_Light.
+ 
+ Wifly_Light is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Wifly_Light is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Wifly_Light.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include "ledstrip.h"
+#include "unittest.h"
+
+#define NUM_TEST_LOOPS 255
+
+
+/** Dummies and wrapper functions */
+#include "ScriptCtrl.h"
+void SPI_Init(void) {};
+void SPI_SendLedBuffer(void) {};
+struct ScriptBuf gScriptBuf;
+
+
+int ut_Ledstrip_Init(void)
+{
+	TestCaseBegin();
+
+	TestCaseEnd();
+}
+
+
+
+int main(int argc, const char* argv[])
+{
+	UnitTestMainBegin();
+	RunTest(true, ut_Ledstrip_Init);
+	UnitTestMainEnd();
+}
+
