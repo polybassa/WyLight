98a244ff9b443cc79bf6317b19cdd56738498e33
gpb <taw@bruenn.biz>
2013-01-06 23:30:32 +0100

    android client is now able to update the remotes dynamically

--- android/WiflyLight/src/biz/bruenn/WiflyLight/WiflyLightActivity.java
+++ android/WiflyLight/src/biz/bruenn/WiflyLight/WiflyLightActivity.java
@@ -3,21 +3,17 @@ package biz.bruenn.WiflyLight;
 import java.util.ArrayList;
 
 import biz.bruenn.WiflyLight.R.id;
+import biz.bruenn.WiflyLight.R.string;
 import android.app.Activity;
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class WiflyLightActivity extends Activity {
-	
-	private RemoteCollector mRemoteCollector;
 	private ListView mRemoteList;
 	private ArrayList<String> mRemoteArray = new ArrayList<String>();
 	ArrayAdapter<String> mRemoteArrayAdapter;
@@ -32,33 +28,21 @@ public class WiflyLightActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        mRemoteArray.add("1");
-        mRemoteArray.add("2");
         mRemoteList = (ListView)findViewById(id.remoteList);
         mRemoteArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mRemoteArray);
-        mRemoteCollector = new RemoteCollector(mRemoteArrayAdapter);
         mRemoteList.setAdapter(mRemoteArrayAdapter);
-       
         
         Button scanBtn = (Button)findViewById(id.scan);
         scanBtn.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
-				WifiManager wifiMgr = (WifiManager)getSystemService(Context.WIFI_SERVICE);
-				if(wifiMgr != null) {
-					WifiManager.MulticastLock lock = wifiMgr.createMulticastLock("WiflyLight_MulticastLock");
-					lock.acquire();
-
-					ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-					NetworkInfo info = connMgr.getActiveNetworkInfo();
-					TextView debug = (TextView)findViewById(R.id.debugOutput);
-					if(null != info && info.isConnected()) {
-						mRemoteCollector.run();
-					} else {
-						debug.setText("no network connection found");
-					}
-					lock.release();
-				}
+				Button btn = (Button)v;
+				btn.setClickable(false);
+				btn.setText(string.scanning);
+				new RemoteCollector((WifiManager)getSystemService(Context.WIFI_SERVICE), 
+						mRemoteArray,
+						mRemoteArrayAdapter,
+						btn).execute(Long.valueOf(3000));
 			}
 		});
     }
