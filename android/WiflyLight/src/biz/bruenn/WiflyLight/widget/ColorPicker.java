ccb1486a491abcf8560c36bcab9263443a4045e6
gpb <taw@bruenn.biz>
2013-01-19 19:17:22 +0100

    implemented generic sector generation TODO: set sectorWidth dynamically

--- android/WiflyLight/src/biz/bruenn/WiflyLight/widget/ColorPicker.java
+++ android/WiflyLight/src/biz/bruenn/WiflyLight/widget/ColorPicker.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.Shader;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RectShape;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,47 +17,35 @@ public class ColorPicker extends View {
 		public void onColorChange(int red);
 	};
 
-	private static final int SECTOR_NUM = 3; 
-	private static final int SECTOR_WIDTH = 50; 
+	private static final int SECTOR_NUM = 6; 
+	private final int mSectorWidth; 
 	
-	private ShapeDrawable mDrawable;
-	private ArrayList<LinearGradient> mGradient;
-	private LinearGradient mGradient_2;
-	private LinearGradient mGradient_3;
+	private ArrayList<LinearGradient> mGradient = new ArrayList<LinearGradient>(SECTOR_NUM);
+	private Paint mPaint = new Paint();
+	private ArrayList<Rect> mRect = new ArrayList<Rect>(SECTOR_NUM);
 	private OnColorChangeListener mOnColorChangeListener;
+	private int mSectorBasecolor[] = {
+			0xffff0000,
+			0xffff00ff, 
+			0xff0000ff,
+			0xff00ffff,
+			0xff00ff00,
+			0xffffff00,
+			0xffff0000};
 
 	public ColorPicker(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		
-		mGradient = new ArrayList<LinearGradient>(SECTOR_NUM);
-		int x = 0;
-		int y = 0;
-		int width = 200;
-		int height = 200;
-		mDrawable = new ShapeDrawable(new RectShape());
-		mDrawable.getPaint().setARGB(0xff, 0xff, 0, 0);
-		mDrawable.setBounds(x, y, x+width, y + height);
-		
+		mSectorWidth = 50;//this.getWidth() / SECTOR_NUM;
+		final int y = 0;
+		for(int sector = 0; sector < SECTOR_NUM; sector++) {
+			final int x = sector*mSectorWidth;
+			mGradient.add(new LinearGradient(x, y, x + mSectorWidth, y,
+					new int[]{mSectorBasecolor[sector], mSectorBasecolor[sector+1]}, null, Shader.TileMode.CLAMP));
+			mRect.add(new Rect(x, 0, x + mSectorWidth, 200));
+		}
 
-		mGradient.add(new LinearGradient(0, y, 50, y,
-				new int[]{0xffff0000, 0xffff00ff}, null, Shader.TileMode.CLAMP));
-		
-		mGradient.add(new LinearGradient(50, y, 100, y,
-				new int[]{0xffff00ff, 0xff00ffff}, null, Shader.TileMode.CLAMP));
-		mGradient.add(new LinearGradient(100, y, 150, y,
-				new int[]{0xff00ffff, 0xff00ff00}, null, Shader.TileMode.CLAMP));
-
-		this.setOnTouchListener(new View.OnTouchListener() {
-			//TODO make this static
-			final int mSectorBasecolor[] = {
-				0xffff0000,
-				0xffff00ff, 
-				0xff0000ff,
-				0xff00ffff,
-				0xff00ff00,
-				0xffffff00};
-			
-			
+		this.setOnTouchListener(new View.OnTouchListener() {			
 			public boolean onTouch(View v, MotionEvent event) {
 				final int sectorWidth = (v.getWidth() / 6) + 1;
 				final int pixelX = (int)event.getX();
@@ -84,14 +70,12 @@ public class ColorPicker extends View {
 	}
 	
 	protected void onDraw(Canvas canvas) {
-		Paint p = new Paint();
-		
-		p.setShader(mGradient.get(0));
-		canvas.drawRect(0, 0, 50, 200, p);
-		p.setShader(mGradient.get(1));
-		canvas.drawRect(50, 0, 100, 200, p);
-		p.setShader(mGradient.get(2));
-		canvas.drawRect(100, 0, 150, 200, p);
+		int index = 0;
+		for(LinearGradient lg: mGradient) {
+			mPaint.setShader(lg);
+			canvas.drawRect(mRect.get(index), mPaint);
+			index++;
+		}		
 	}
 	
 	public void setOnColorChangeListener(OnColorChangeListener l) {
