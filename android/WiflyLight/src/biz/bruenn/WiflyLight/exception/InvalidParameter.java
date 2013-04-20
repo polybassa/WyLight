f270632da924d2c1dce102a8f8ff15aced419e78
gpb <taw@bruenn.biz>
2013-04-20 20:24:49 +0200

    added lost android exception files :-(

---
+++ android/WiflyLight/src/biz/bruenn/WiflyLight/exception/InvalidParameter.java
@@ -0,0 +1,11 @@
+package biz.bruenn.WiflyLight.exception;
+
+public class InvalidParameter extends FatalError {
+
+	private static final long serialVersionUID = 7810409241984695478L;
+
+	InvalidParameter(String s) {
+		super("InvalidParameter: " + s);
+	}
+
+}
