59d477839d88b23a5f8428feaf39b72ada4a72cc
Nils Weiß <n125.we155@googlemail.com>
2013-10-26 13:34:42 +0200

    change command model, add needs update variable

---
+++ WyLight/WyLight/CommandModel/NWScript+Snapshot.h
@@ -0,0 +1,13 @@
+//
+//  NWScript+Snapshot.h
+//  WyLight
+//
+//  Created by Nils Weiß on 25/10/13.
+//  Copyright (c) 2013 Nils Weiß. All rights reserved.
+//
+
+#import "NWScript.h"
+
+@interface NWScript (Snapshot)
+
+@end
---
+++ WyLight/WyLight/CommandModel/NWScript+Snapshot.m
@@ -0,0 +1,13 @@
+//
+//  NWScript+Snapshot.m
+//  WyLight
+//
+//  Created by Nils Weiß on 25/10/13.
+//  Copyright (c) 2013 Nils Weiß. All rights reserved.
+//
+
+#import "NWScript+Snapshot.h"
+
+@implementation NWScript (Snapshot)
+
+@end
