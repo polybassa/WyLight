9b3609f7dd59291040f9f169113a4126419a9e80
gpb <taw@bruenn.biz>
2014-01-14 16:49:49 +0100

    - optimizing hsv calculation

--- android/WiflyLight/src/biz/bruenn/WyLight/SetColorFragment.java
+++ android/WiflyLight/src/biz/bruenn/WyLight/SetColorFragment.java
@@ -33,6 +33,7 @@ public class SetColorFragment extends ControlFragment implements OnColorChangeLi
 	// These values only have to be recalculated, if the views layout changes
 	private float mCenterX = 0;
 	private float mCenterY = 0;
+	private float mCenterZ = 0;
 	private int mDiameter = 0;
 	private int mRadius = 0;
 	private int mRadiusCrosshair = 0;
@@ -46,29 +47,31 @@ public class SetColorFragment extends ControlFragment implements OnColorChangeLi
 
 	private View.OnTouchListener mOnTouchListener = new View.OnTouchListener() {
 		public boolean onTouch(View v, MotionEvent event) {
+			if(event.getAction() != MotionEvent.ACTION_MOVE) {
+				return true;
+			}
 			if(v.equals(mColorPicker)) {
 				if(!mChangeIsInProgress.getAndSet(true)) {
 					int x = Math.max(0, Math.min(mDiameter-1, (int)event.getX()));
 					int y = Math.max(0, Math.min(mDiameter-1, (int)event.getY()));
-					double x0 = x - mRadius;
-					double y0 = y - mRadius;
-					final double factor = (double)mRadius / Math.hypot(x0, y0);
-					if (factor < 1d) {
-						x0 *= factor;
-						y0 *= factor;
-						x = (int) Math.round(x0 + mRadius);
-						y = (int) Math.round(y0 + mRadius);
-					}
+					final int x0 = x - mRadius;
+					final int y0 = y - mRadius;
+					final float saturation = (float)Math.min(1d, Math.hypot(x0, y0) / mRadius);
 
-					final float[] hsv = new float[3];
-					coordinateToHSV(v.getX() + x - mRadiusCrosshair, v.getY() + y - mRadiusCrosshair, hsv);
-					mProvider.setColorHueSaturation(hsv[0], hsv[1]);
+					final double x1 = x - v.getWidth()/2;
+					final double y1 = y - v.getWidth()/2;
+
+					final double magnitude = Math.hypot(x1, y1);
+					final double alpha = _180_DIVIDED_PI * Math.acos(x1/magnitude);
+					final float hue = (float)((y1 < 0) ? alpha : 360 - alpha);
+
+					mProvider.setColorHueSaturation(hue, saturation);
 					mChangeIsInProgress.set(false);
 				}
 			} else if (v.equals(mValuePicker)) {
 				if(!mChangeIsInProgress.getAndSet(true)) {
-					float z = Math.max(v.getX(), Math.min(v.getX() + v.getWidth(), event.getX()));
-					mProvider.setColorValue(coordinateToValue(z - mRadiusCrosshair));
+					final float value = Math.max(0f, Math.min(1f, event.getX() / v.getWidth()));
+					mProvider.setColorValue(value);
 					mChangeIsInProgress.set(false);
 				}
 			} else {
@@ -95,11 +98,6 @@ public class SetColorFragment extends ControlFragment implements OnColorChangeLi
 		hsv[2] = 1f;
 	}
 
-	private float coordinateToValue(float z) {
-		final float crosshairCenterZ = z + this.mRadiusCrosshair;
-		return (crosshairCenterZ - mValuePicker.getX()) / (float)mValuePicker.getWidth();
-	}
-
 	@Override
 	public int getIcon() {
 		return R.drawable.ic_action_location_searching;
@@ -108,8 +106,8 @@ public class SetColorFragment extends ControlFragment implements OnColorChangeLi
 	public void onColorChanged(float[] hsv) {
 		if(null != mColorStatus) {
 			mColorStatus.setColor(Color.HSVToColor(hsv));
-			setCrosshair(hsv);
-			setValueGradient(hsv.clone());
+			setCrosshairs(hsv);
+			setValueGradient(hsv);
 		}
 	}
 
@@ -135,18 +133,19 @@ public class SetColorFragment extends ControlFragment implements OnColorChangeLi
 			mProvider.removeOnColorChangedListener(this);
 			return;
 		}
-		final int shift = (mColorPicker.getWidth() - mCrosshair.getWidth()) / 2;
-		mCenterX = mColorPicker.getX() + shift;
-		mCenterY = mColorPicker.getY() + shift;
 		mDiameter = mColorPicker.getWidth();
 		mRadius = mDiameter / 2;
 		mRadiusCrosshair = mCrosshair.getWidth() / 2;
+		final int shift = (mColorPicker.getWidth() - mCrosshair.getWidth()) / 2;
+		mCenterX = mColorPicker.getX() + shift;
+		mCenterY = mColorPicker.getY() + shift;
+		mCenterZ = mValuePicker.getX() -mRadiusCrosshair;
 		mColorPicker.setOnTouchListener(mOnTouchListener);
 		mValuePicker.setOnTouchListener(mOnTouchListener);
 		mProvider.addOnColorChangedListener(this);
 	}
 
-	private void setCrosshair(float[] hsv) {
+	private void setCrosshairs(float[] hsv) {
 		final double hue = hsv[0] * PI_DIVIDED_MINUS_180;
 		final double saturation = hsv[1] * mRadius;
 		final float value = hsv[2] * mValuePicker.getWidth();
@@ -154,14 +153,18 @@ public class SetColorFragment extends ControlFragment implements OnColorChangeLi
 		final double y0 = Math.sin(hue) * saturation;
 		mCrosshair.setX(mCenterX + (float)x0);
 		mCrosshair.setY(mCenterY + (float)y0);
-		mValueCrosshair.setX(mValuePicker.getX() + value - mRadiusCrosshair);
+		mValueCrosshair.setX(mCenterZ + value);
 	}
 
-	private void setValueGradient(float[] hsv) {
-		hsv[2] = 1f;
-		ShapeDrawable mDrawable = new ShapeDrawable(new RectShape());
+	/**
+	 * updates the hsv value pickers background gradient
+	 * @param hsv should be an array of three floats: hsv[0] is Hue [0 .. 360) hsv[1] is Saturation [0...1] hsv[2] is Value [0...1]
+	 */
+	private void setValueGradient(float[] ref) {
+		final float[] hsv = new float[]{ref[0], ref[1], 1f};
+		ShapeDrawable background = new ShapeDrawable(new RectShape());
 		LinearGradient gradient = new LinearGradient(0, 0, mValuePicker.getWidth(), 0, Color.BLACK, Color.HSVToColor(hsv), Shader.TileMode.CLAMP);
-		mDrawable.getPaint().setShader(gradient);
-		mValuePicker.setBackgroundDrawable(mDrawable);
+		background.getPaint().setShader(gradient);
+		mValuePicker.setBackgroundDrawable(background);
 	}
 }
