3bb9fb5b2e06c9ba018dc42bddbf219bbcd327bd
Nils Weiß <n125.we155@googlemail.com>
2014-02-18 12:42:57 +0100

    webpage edit

---
+++ wylight.de/WyLightWebpage/public_html/js/smoothscroll.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
