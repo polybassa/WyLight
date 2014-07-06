c2068fa253ce9f476aa1ae688fb36c75b0300884
gpb <taw@bruenn.biz>
2014-07-06 17:12:36 +0200

    fix unittests

--- library/TelnetProxy_ut.cpp
+++ library/TelnetProxy_ut.cpp
@@ -48,7 +48,7 @@ void SetDelay(timeval& delay)
 	g_TestSocketSendDelay.tv_nsec = delay.tv_usec * 1000;
 }
 
-TcpSocket::TcpSocket(uint32_t addr, uint16_t port) throw (ConnectionLost, FatalError) : ClientSocket(addr, port, 0)
+TcpSocket::TcpSocket(int listenSocket, const struct timespec *timeout) throw (ConnectionLost, FatalError) : ClientSocket(0, 0, 0)
 {
 	g_TestSocketSendDelay.tv_sec = 0;
 	g_TestSocketSendDelay.tv_nsec = 0;
