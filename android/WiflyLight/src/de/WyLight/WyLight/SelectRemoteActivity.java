f966ff6ddfa354e3cc6e3e1834b85e77b1dbfa54
gpb <taw@bruenn.biz>
2014-08-07 23:13:58 +0200

    add copyright notice to java files

--- android/WiflyLight/src/de/WyLight/WyLight/SelectRemoteActivity.java
+++ android/WiflyLight/src/de/WyLight/WyLight/SelectRemoteActivity.java
@@ -1,7 +1,24 @@
+/**
+                Copyright (C) 2012 - 2014 Patrick Bruenn.
+
+    This file is part of WyLight.
+
+    Wylight is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WiflyLight is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WiflyLight.  If not, see <http://www.gnu.org/licenses/>. */
+
 package de.WyLight.WyLight;
 
 import java.util.ArrayList;
-import java.util.ListIterator;
 
 import de.WyLight.WyLight.R;
 import de.WyLight.WyLight.R.id;
@@ -9,7 +26,6 @@ import de.WyLight.WyLight.R.string;
 import de.WyLight.WyLight.library.Endpoint;
 import de.WyLight.WyLight.library.EndpointAdapter;
 import android.app.Activity;
-import android.app.Dialog;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
@@ -97,11 +113,6 @@ public class SelectRemoteActivity extends Activity implements
 	@Override
 	protected void onResume() {
 		super.onResume();
-		ListIterator<Endpoint>e = mRemoteArray.listIterator();
-		while(e.hasNext()) {
-			e.next().setOnline(false);
-		}
-		mRemoteArrayAdapter.notifyDataSetChanged();
 		scan();
 	}
 
