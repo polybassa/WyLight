876155db9783764cd6471ce2263f13bad74fc561
Patrick Brünn <patrick@wylight.de>
2014-09-04 20:19:17 +0200

    move TcpServer into separate file remove more annoying empty comment lines

---
+++ cc3200_gcc/bl/bootloader.h
@@ -0,0 +1,22 @@
+/*
+ Copyright (C) 2014 Nils Weiss, Patrick Bruenn.
+
+ This file is part of WyLight.
+
+ WyLight is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ WyLight is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with WyLight.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef _BOOTLOADER_H_
+#define _BOOTLOADER_H_
+extern const unsigned long BOOTLOADER_VERSION;
+#endif /* #ifndef _BOOTLOADER_H_ */
