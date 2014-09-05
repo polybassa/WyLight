fc079e6ce7a48f3f7a923aae3c2b0ce81d13b898
Nils Weiß <n125.we155@googlemail.com>
2014-09-05 21:00:58 +0200

    move simulator.h in right folder

--- cc3200_gcc/sim/simulator.h
+++ cc3200_gcc/sim/simulator.h
@@ -19,6 +19,32 @@
 #ifndef __SIMULATOR_H_
 #define __SIMULATOR_H_
 
+#include <stdint.h>
+#include <stdbool.h>
 
+#define SUCCESS 0
+
+#define MAP_IntVTableBaseSet(x)
+#define MAP_IntMasterEnable()
+#define MAP_IntEnable(x)
+#define PRCMCC3200MCUInit()
+#define PRCMPeripheralClkEnable(x,y)
+#define MAP_SHAMD5IntRegister(x,y)
+#define PRCMPeripheralReset(x)
+#define SHAMD5ConfigSet(x,y)
+#define MAP_IntDisable(x)
+#define MAP_IntMasterDisable()
+
+uint32_t MAP_SHAMD5IntStatus(uint32_t base, bool flag);
+
+void MAP_SHAMD5IntDisable(uint32_t base, uint32_t state);
+
+void SHAMD5IntEnable(uint32_t base, uint32_t flags);
+
+void SHAMD5DataLengthSet(uint32_t base, uint32_t length);
+
+void SHAMD5DataWrite(uint32_t base, uint8_t *pSource);
+
+void SHAMD5ResultRead(uint32_t base, uint8_t *pDest);
 
 #endif /* __SIMULATOR_H_ */
