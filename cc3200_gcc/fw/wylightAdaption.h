7a2d1b4b50db880cf0a226502d3d89d3b075b4e4
Nils Weiß <n125.we155@googlemail.com>
2014-09-06 14:56:23 +0200

    add copyright notice

--- cc3200_gcc/fw/wylightAdaption.h
+++ cc3200_gcc/fw/wylightAdaption.h
@@ -1,15 +1,20 @@
-//*****************************************************************************
-// Copyright (C) 2014 Texas Instruments Incorporated
-//
-// All rights reserved. Property of Texas Instruments Incorporated.
-// Restricted rights to use, duplicate or disclose this code are
-// granted through contract.
-// The program may not be used without the written permission of
-// Texas Instruments Incorporated or against the terms and conditions
-// stipulated in the agreement under which this program has been supplied,
-// and under no circumstances can it be used with non-TI connectivity device.
-//
-//*****************************************************************************
+/*
+ Copyright (C) 2014 Nils Weiss, Patrick Bruenn.
+ 
+ This file is part of Wifly_Light.
+ 
+ Wifly_Light is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Wifly_Light is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Wifly_Light.  If not, see <http://www.gnu.org/licenses/>. */
 
 // Simplelink includes
 #ifndef extern
