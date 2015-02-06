55ad30b304c3ac792e78c3aa71d67050f04ba1b9
Nils Weiß <n125.we155@googlemail.com>
2015-02-05 20:56:18 -0800

    fix debug function

--- common/uart_if.c
+++ common/uart_if.c
@@ -58,6 +58,7 @@
 
 #include "uart.h"
 #include "uart_if.h"
+#include "rom_map.h"
 
 //*****************************************************************************
 //
@@ -72,7 +73,7 @@
 void 
 InitTerm()
 {
-#ifdef DEBUG
+#ifdef WY_DEBUG
   MAP_UARTConfigSetExpClk(CONSOLE,MAP_PRCMPeripheralClockGet(CONSOLE_PERIPH), 
                   UART_BAUD_RATE, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
                    UART_CONFIG_PAR_NONE));
@@ -94,7 +95,7 @@ InitTerm()
 void 
 Message(const char *str)
 {
-#ifdef DEBUG
+#ifdef WY_DEBUG
     if(str != NULL)
     {
         while(*str)
@@ -139,7 +140,7 @@ ClearTerm()
 int Report(const char *pcFormat, ...)
 {
  int iRet = 0;
-#ifdef DEBUG
+#ifdef WY_DEBUG
   char pcBuff[256];
   int iSize = sizeof(pcBuff);
  
