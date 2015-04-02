c0b4fc1693390cfe03f52bc2f1b7d25f97c6906d
Nils Weiß <n125.we155@googlemail.com>
2015-04-02 12:34:15 +0200

    small improvements on build script

--- tools/autobuild.sh
+++ tools/autobuild.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# check arguments
-if [ $# -eq 0 ] || ( [ "$1" != "nightly" ] && [ "$1" != "build" ] ); then
-	echo -e "Usage:\n\t$0 nightly -> preserve build results"
-	echo -e "or\n\t$0 build   -> delete build results\n"
-    exit 1
-fi
-
 SENDMAIL=/usr/sbin/sendmail
 
 BUILD_TYPE=$1
@@ -13,40 +6,59 @@ BUILD_BRANCH=${BUILD_TYPE}-$(date +%Y%m%d%H%M%S)
 PULL_BRANCH=Master
 BINDIR=exe
 MAIL_HEADER="to: admin@wylight.de\nfrom: build01.wylight@gmail.com\nsubject: ${BUILD_BRANCH}"
+LOGFILE=buildlog.txt
+TARGETS=(test cc_fw firmware_pic cli cc_bl sim library)
+
+
+function make_targets() {
+	# run tests
+	for target in $TARGETS
+	do
+		echo -e "\n\n Start to build $target \n\n---------------------------------------\n\n" >>$LOGFILE
+		make $target >> $LOGFILE
+
+		if [ $? -eq 0 ]; then
+			return 1
+		fi
+	done
+	return 0
+}
+
+function report_result() {
+	echo $0 >> $LOGFILE
+	echo -e "${MAIL_HEADER} $0\n\n" > buildmsg.txt
+	cat $LOGFILE >> buildmsg.txt
+	cat buildmsg.txt | ${SENDMAIL} -t
+	rm buildmsg.txt
+	cat $LOGFILE
+}
+
+# check arguments
+if [ $# -eq 0 ] || ( [ "$1" != "nightly" ] && [ "$1" != "build" ] ); then
+	echo -e "Usage:\n\t$0 nightly -> preserve build results"
+	echo -e "or\n\t$0 build   -> delete build results\n"
+    exit 1
+fi
 
 # prepare a fresh branch for build
 cd WyLight && \
-git checkout ${PULL_BRANCH} > buildlog.txt && \
-git pull >> buildlog.txt && \
-git checkout -b ${BUILD_BRANCH} >> buildlog.txt && \
-./configure >> buildlog.txt
+git checkout ${PULL_BRANCH} > $LOGFILE && \
+git pull >> $LOGFILE && \
+git checkout -b ${BUILD_BRANCH} >> $LOGFILE && \
+echo -e "\n\nSTART CONFIGURATION\n\n--------------------\n\n" >> $LOGFILE &&\
+./configure >> $LOGFILE
+
 if [ $? -ne 0 ]; then
-	echo "prepare repository and branch for build failed" >> buildlog.txt
-	echo -e "${MAIL_HEADER} prepare repository failed\n" > buildmsg.txt
-	cat buildlog.txt >> buildmsg.txt
-	cat buildmsg.txt | ${SENDMAIL} -t
+	report_result "prepare repository and branch for build failed"
 	exit 1
 fi
 
-# run tests
-# TODO replace this with 'make all' when supported on raspi
-make firmware_test >> buildlog.txt && \
-make cli >>  buildlog.txt && \
-make firmware_pic >> buildlog.txt
-
-if [ $? -ne 0 ]; then
-	echo "build failed!" >> buildlog.txt
-	echo -e "${MAIL_HEADER} failed\n" > buildmsg.txt
-	cat buildlog.txt >> buildmsg.txt
-	cat buildmsg.txt | ${SENDMAIL} -t
-	exit $?
+if [ make_targets -ne 0 ]; then
+	report_result "build failed!"
+	exit 1
 fi
 
-echo "build was successful!" >> buildlog.txt
-echo -e "${MAIL_HEADER} was successful\n" > buildmsg.txt
-cat buildlog.txt >> buildmsg.txt
-cat buildmsg.txt | ${SENDMAIL} -t
-
+report_result "build was successful!"
 
 # save nightly build results only
 if [ "${BUILD_TYPE}" == "nightly" ]; then
