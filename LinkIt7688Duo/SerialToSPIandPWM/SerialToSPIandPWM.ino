0618768d5dc3d257065acbc26cd3ccbe48ed86b2
Patrick Brünn <patrick@wylight.de>
2016-09-10 21:24:10 +0200

    linkit: run uncrustify
    add linkit firmware files to uncrustify filelist

--- LinkIt7688Duo/SerialToSPIandPWM/SerialToSPIandPWM.ino
+++ LinkIt7688Duo/SerialToSPIandPWM/SerialToSPIandPWM.ino
@@ -29,132 +29,131 @@ static const uint32_t PWM_FREQ = 100 * 1000UL;
 static const size_t NUM_OF_LED = 32;
 
 static int leds[3 * NUM_OF_LED] {32, 64, 128};
-static uint32_t  g_pwm_period;
+static uint32_t g_pwm_period;
 
-static void write10BitReg(volatile uint8_t *reg, uint16_t value)
+static void write10BitReg(volatile uint8_t* reg, uint16_t value)
 {
-  TC4H = (value & 0x03ff) >> 8;
-  *reg = (value & 0x00ff);
+    TC4H = (value & 0x03ff) >> 8;
+    *reg = (value & 0x00ff);
 }
 
 static void init_pll()
 {
-  static const uint8_t PLLFRQ_96_MHZ_OUTPUT = _BV(PDIV1) | _BV(PDIV3);
-  static const uint8_t PLLFRQ_64_MHZ_TIMER4 = _BV(PLLTM1);
+    static const uint8_t PLLFRQ_96_MHZ_OUTPUT = _BV(PDIV1) | _BV(PDIV3);
+    static const uint8_t PLLFRQ_64_MHZ_TIMER4 = _BV(PLLTM1);
 
-  /* select frequencies */
-  PLLFRQ = PLLFRQ_96_MHZ_OUTPUT | PLLFRQ_64_MHZ_TIMER4;
+    /* select frequencies */
+    PLLFRQ = PLLFRQ_96_MHZ_OUTPUT | PLLFRQ_64_MHZ_TIMER4;
 
-  /* enable PLL and wait until unlocked */
-  PLLCSR = _BV(PLLE);
-  while(!(PLLCSR & _BV(PLOCK)));
+    /* enable PLL and wait until unlocked */
+    PLLCSR = _BV(PLLE);
+    while (!(PLLCSR & _BV(PLOCK))) {}
 }
 
 static void init_timer4_pwm()
 {
-  uint8_t prescaler = 1;
+    uint8_t prescaler = 1;
 
-  /* enable output compare on port A, B and D*/
-  TCCR4A = _BV(PWM4A) | _BV(PWM4B);
-  TCCR4C = _BV(PWM4D);
+    /* enable output compare on port A, B and D*/
+    TCCR4A = _BV(PWM4A) | _BV(PWM4B);
+    TCCR4C = _BV(PWM4D);
 
-  /* enable enhanced mode with more accuracy in OCR4x registers */
-  TCCR4E = _BV(ENHC4);
+    /* enable enhanced mode with more accuracy in OCR4x registers */
+    TCCR4E = _BV(ENHC4);
 
-  /* enable phase and frequency correct PWM mode */
-  TCCR4D = _BV(WGM40);
+    /* enable phase and frequency correct PWM mode */
+    TCCR4D = _BV(WGM40);
 
-  /* calculate prescaling and period */
-  g_pwm_period = PLL_FREQ_64MHZ / 2 / PWM_FREQ;
-  while ((g_pwm_period >= TIMER4_RESOLUTION) && (prescaler < 0x10)) {
-    g_pwm_period >>= 1;
-    ++prescaler;
-  }
+    /* calculate prescaling and period */
+    g_pwm_period = PLL_FREQ_64MHZ / 2 / PWM_FREQ;
+    while ((g_pwm_period >= TIMER4_RESOLUTION) && (prescaler < 0x10)) {
+        g_pwm_period >>= 1;
+        ++prescaler;
+    }
 
-  /* configure prescaling for timer4 */
-  TCCR4B = prescaler & 0x0F;
+    /* configure prescaling for timer4 */
+    TCCR4B = prescaler & 0x0F;
 
-  /* configure TOP for timer4 */
-  write10BitReg(&OCR4C, g_pwm_period);
+    /* configure TOP for timer4 */
+    write10BitReg(&OCR4C, g_pwm_period);
 }
 
 static void pwm_enable()
 {
-  DDRB |= _BV(PORTB6);
-  DDRC |= _BV(PORTC7);
-  DDRD |= _BV(PORTD7);
-  TCCR4A |= _BV(COM4A1) | _BV(COM4B1);
-  TCCR4C |= _BV(COM4D1);
+    DDRB |= _BV(PORTB6);
+    DDRC |= _BV(PORTC7);
+    DDRD |= _BV(PORTD7);
+    TCCR4A |= _BV(COM4A1) | _BV(COM4B1);
+    TCCR4C |= _BV(COM4D1);
 }
 
 static void pwm_disable()
 {
-  DDRB &= ~(_BV(PORTB6));
-  DDRC &= ~(_BV(PORTC7));
-  DDRD &= ~(_BV(PORTD7));
-  TCCR4A &= ~(_BV(COM4A1) | _BV(COM4B1));
-  TCCR4C &= ~(_BV(COM4D1));
+    DDRB &= ~(_BV(PORTB6));
+    DDRC &= ~(_BV(PORTC7));
+    DDRD &= ~(_BV(PORTD7));
+    TCCR4A &= ~(_BV(COM4A1) | _BV(COM4B1));
+    TCCR4C &= ~(_BV(COM4D1));
 }
 
-static void pwm_set_duty(volatile uint8_t *reg, uint16_t duty)
+static void pwm_set_duty(volatile uint8_t* reg, uint16_t duty)
 {
-   uint32_t dutyCycle = g_pwm_period * duty;
-   dutyCycle >>= 9;
-   write10BitReg(reg, dutyCycle);
+    uint32_t dutyCycle = g_pwm_period * duty;
+    dutyCycle >>= 9;
+    write10BitReg(reg, dutyCycle);
 }
 
 static void leds_update()
 {
-  static volatile uint8_t *const reg[3] = {
-    &OCR4A,
-    &OCR4B,
-    &OCR4D
-  };
-  static uint32_t last[3] = {0};
+    static volatile uint8_t* const reg[3] = {
+        &OCR4A,
+        &OCR4B,
+        &OCR4D
+    };
+    static uint32_t last[3] = {0};
 
-  for (size_t i = 0; i < 3; ++i) {
-    if (last[i] != leds[i]) {
-      last[i] = leds[i];
-      pwm_set_duty(reg[i], leds[i] << 2);
+    for (size_t i = 0; i < 3; ++i) {
+        if (last[i] != leds[i]) {
+            last[i] = leds[i];
+            pwm_set_duty(reg[i], leds[i] << 2);
+        }
     }
-  }
 }
 
 void setup()
 {
-  init_pll();
-  init_timer4_pwm();
-  pwm_enable();
+    init_pll();
+    init_timer4_pwm();
+    pwm_enable();
 
-  SPI.begin();
+    SPI.begin();
 
-  leds_update();
-  Serial1.begin(230400);
+    leds_update();
+    Serial1.begin(230400);
 }
 
 void loop()
 {
-  static const unsigned long timeout = 1000;
-  static auto lastUpdate = millis();
-  static size_t next = 0;
+    static const unsigned long timeout = 1000;
+    static auto lastUpdate = millis();
+    static size_t next = 0;
 
-  if (millis() - lastUpdate > 1000) {
-    while (Serial1.available()) {
-      Serial1.read();
+    if (millis() - lastUpdate > 1000) {
+        while (Serial1.available()) {
+            Serial1.read();
+        }
+        lastUpdate = millis();
+        next = 0;
     }
-    lastUpdate = millis();
-    next = 0;
-  }
 
-  if (Serial1.available() > 2) {
-    leds[next++] = Serial1.read();
-    leds[next++] = Serial1.read();
-    leds[next++] = Serial1.read();
+    if (Serial1.available() > 2) {
+        leds[next++] = Serial1.read();
+        leds[next++] = Serial1.read();
+        leds[next++] = Serial1.read();
 
-    next = next % ARRAY_SIZE(leds);
-    if (!next) {
-      //leds_update();
-      lastUpdate = millis();
+        next = next % ARRAY_SIZE(leds);
+        if (!next)
+            //leds_update();
+            lastUpdate = millis();
     }
-  }
 }
