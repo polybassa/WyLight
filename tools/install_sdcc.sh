0ecf5fcce959b83376710d4e6cd951db78692bf9
Patrick Brünn <patrick@wylight.de>
2016-11-01 19:06:19 +0100

    tools: fix sdcc install script

--- tools/install_sdcc.sh
+++ tools/install_sdcc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
+sudo apt-get install flex gputils libboost-graph-dev subversion
 svn checkout svn://svn.code.sf.net/p/sdcc/code/trunk/sdcc sdcc
-sudo apt-get install libboost-graph-dev
 pushd sdcc
-./configure
-make
+./configure --disable-mcs51-port --disable-z80-port --disable-z180-port --disable-r2k-port --disable-r3ka-port --disable-gbz80-port --disable-tlcs90-port --disable-ds390-port --disable-ds400-port --disable-pic14-port --disable-hc08-port --disable-s08-port --disable-stm8-port
+make `nproc`
