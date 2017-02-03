01b41eebf0a7b34b00688f5c8c50cb66611b36dc
Patrick Brünn <patrick@wylight.de>
2017-02-03 21:23:00 +0100

    example: replace hardcoded destination

--- examples/shell_script_example.sh
+++ examples/shell_script_example.sh
@@ -1,3 +1,12 @@
-echo -e "clear\nsetfade ff ff0000 0 0\nexit" | ./exe/WyLight_cli.bin -c 192.168.43.142
-echo -e "loopon\nsetmove 1\nexit" | ./exe/WyLight_cli.bin -c 192.168.43.142
-echo -e "wait 500\nloopoff 0\nexit" | ./exe/WyLight_cli.bin -c 192.168.43.142
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo -e "usage: $0 <wylight>\nf.e.: $0 192.168.43.142\n"
+	exit 1
+fi
+
+destination=$1
+
+echo -e "clear\nsetfade ff ff0000 0 0\nexit" | ./exe/WyLight_cli.bin -c ${destination}
+echo -e "loopon\nsetmove 1\nexit" | ./exe/WyLight_cli.bin -c ${destination}
+echo -e "wait 500\nloopoff 0\nexit" | ./exe/WyLight_cli.bin -c ${destination}
