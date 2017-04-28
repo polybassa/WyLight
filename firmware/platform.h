0d3883101a65b7406fc26d561ed4bc3282ec439d
Patrick Brünn <patrick@wylight.de>
2017-04-28 10:13:00 +0200

    update copyright notice for changed files

--- firmware/platform.h
+++ firmware/platform.h
@@ -1,20 +1,20 @@
 /*
-   Copyright (C) 2012 Nils Weiss, Patrick Bruenn.
+   Copyright (C) 2012 - 2017 Nils Weiss, Patrick Bruenn.
 
-   This file is part of Wifly_Light.
+   This file is part of WyLight.
 
-   Wifly_Light is free software: you can redistribute it and/or modify
+   WyLight is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   Wifly_Light is distributed in the hope that it will be useful,
+   WyLight is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with Wifly_Light.  If not, see <http://www.gnu.org/licenses/>. */
+   along with WyLight.  If not, see <http://www.gnu.org/licenses/>. */
 
 #ifndef _PLATFORM_H_
 #define _PLATFORM_H_
