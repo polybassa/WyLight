cb910196d64be3b0915ef210b464f7df2384c78f
Patrick Brünn <patrick@wylight.de>
2017-06-03 12:00:18 +0200

    esp: switch to esp-open-rtos

--- tools/install_esp_toolchain.sh
+++ tools/install_esp_toolchain.sh
@@ -3,9 +3,11 @@
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 esp_sdk=esp-open-sdk
+esp_rtos=esp-open-rtos
 
 pushd $script_path
 git clone --recursive https://github.com/pfalcon/${esp_sdk}.git
+git clone --recursive https://github.com/Superhouse/${esp_rtos}.git
 
 pushd $esp_sdk
 make
